e884f8ec35a731a9dc1753aa1853db1970a57b39
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-06-08 14:55:52 +0200

    toggle navbar

--- src/components/AboutIntro.jsx
+++ src/components/AboutIntro.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AboutImg from '../images/about.jpg'
-
+import CountUp from 'react-countup'
 const AboutIntro = () => {
   return (
     <div className='header'>
@@ -13,7 +13,24 @@ const AboutIntro = () => {
                 <h3>About Us</h3>
                 <div className='about-content'>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, consequuntur.</p>
-                    <div className="our-status"></div>
+                    <div className="our-status">
+                      <div className='counter p-3'>
+                        <h5 className="counter-title">10</h5>
+                        <p>Completed projects</p>
+                      </div>
+                      <div className='counter p-3'>
+                        <h5 className="counter-title">10</h5>
+                        <p>Completed projects</p>
+                      </div>
+                      <div className='counter p-3'>
+                        <h5 className="counter-title">10</h5>
+                        <p>Completed projects</p>
+                      </div>
+                      <div className='counter p-3'>
+                        <h5 className="counter-title">10</h5>
+                        <p>Completed projects</p>
+                      </div>
+                    </div>
                 </div>
              </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {FaBars, FaTimes} from "react-icons/fa"
 
 const Navbar = () => {
+  const [toggleNav,setToggleNav]=useState(false)
+  const closeNavBar=()=>{
+    setToggleNav(!toggleNav)
+  }
   return (
     <div className='container-fluid navbar d-flex justify-content-space-between '>
        <div className='my-logo'>
         <Link to='/'>Logo</Link>
        </div>
-       <div className='nav-links'>
+      <div className={toggleNav? 'nav-links hide': "nav-links show"}>
             <span><Link to='/'>Home</Link></span>
             <span><Link to='/about'>About</Link></span>
             <span><Link to='/courses'>Courses</Link></span>
             <span><Link to='/pages'>Pages</Link></span>
             <span><Link to='/blog'>Blog</Link></span>
+            
        </div>
+       <div onClick={closeNavBar} className='p-2 close-nav'>
+              {toggleNav? <FaBars /> :    <FaTimes /> }
+            </div>
     </div>
   )
 }
