5bd1ac8696e39a7e3e93a5508887621b0b9235fc
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-08-18 10:37:29 +0200

    completed homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
   }
   return (
     <div className='container-fluid navbar d-flex justify-content-space-between '>
-       <div className='my-logo'>
+       <div className='my-logo' onClick={closeNavBar}>
         <Link to='/'>Logo</Link>
        </div>
       <div className={toggleNav? 'nav-links hide': "nav-links show"}>
-            <span><Link to='/'>Home</Link></span>
-            <span><Link to='/about'>About</Link></span>
-            <span><Link to='/courses'>Courses</Link></span>
-            <span><Link to='/pages'>Pages</Link></span>
-            <span><Link to='/blog'>Blog</Link></span>
+            <span onClick={closeNavBar}><Link to='/'>Home</Link></span>
+            <span onClick={closeNavBar}><Link to='/about'>About</Link></span>
+            <span onClick={closeNavBar}><Link to='/courses'>Courses</Link></span>
+            <span onClick={closeNavBar}><Link to='/pages'>Pages</Link></span>
+            <span onClick={closeNavBar}><Link to='/blog'>Blog</Link></span>
             
        </div>
        <div onClick={closeNavBar} className='p-2 close-nav'>
